bff7d07bb9deb18d29d4a6f2de52302708094655
Michael Rätzel <viir@viir.de>
2016-07-24 18:49:04 +0100

    clean up.

--- src/Sanderling.ABot/Parse/ParseStatic.cs
+++ src/Sanderling.ABot/Parse/ParseStatic.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sanderling.ABot.Parse
+﻿namespace Sanderling.ABot.Parse
 {
 	static public class ParseStatic
 	{
